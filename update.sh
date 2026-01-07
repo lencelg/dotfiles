e6830da72bb0d317bcacb44ea3246cdc05d722da
lancetce <2223544759@qq.com>
2026-01-07 11:32:25 +0800

    update

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dots=("qutebrowser" "foot" "i3status-rust" "niri" "river" "waybar" "wayshot" "yazi" "i3bar-river" "kitty" "mako" "nvim" "way-displays" "wlogout")
+
+# Loop through the list and print each fruit
+for dot in "${dots[@]}"; do
+    rm -rf ~/dotfiles/$dot
+    cp -r ~/.config/$dot ~/dotfiles/
+done
