024abc1f5558cb3db5c551364a73552c6f841578
lancetce <2223544759@qq.com>
2026-02-28 07:37:12 +0800

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dots=("qutebrowser" "foot"  "niri"  "waybar" "wayshot" "yazi"  "kitty" "mako" "nvim" "way-displays" )
+dots=("qutebrowser" "foot"  "niri"  "waybar" "wayshot" "yazi"  "kitty" "mako" "nvim" )
 
 # Loop through the list and print each fruit
 for dot in "${dots[@]}"; do
