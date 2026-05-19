6e59a240626757bcc67e7de5603d0880279e1734
混的人 <2223544759@qq.com>
2026-05-19 18:52:32 +0800

    adjust keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -29,8 +29,8 @@ vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease wi
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase width' })
 
 -- 行移动
-vim.keymap.set('n', '<A-j>', ':m .+7<CR>==', { desc = 'Move line down' })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-k>', ':m .-1<CR>==', { desc = 'Move line up' })
 vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 
