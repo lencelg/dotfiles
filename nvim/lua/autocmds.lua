6b66b8e5a6096d985a185c9a64279d7aa7ec1a52
混的人 <2223544759@qq.com>
2026-05-21 12:57:52 +0800

    add: disable chinese input when leaving insert mode

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -4,16 +4,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function() vim.highlight.on_yank({ timeout = 500 }) end,
 })
 
--- 为 Java 文件加载 nvim-java
--- vim.api.nvim_create_autocmd('FileType', {
---     pattern = 'java',
---     callback = function()
---         -- 利用 pcall 安全地加载，避免因插件未安装而报错
---         local ok, java = pcall(require, 'java')
---         if ok and java and java.setup then
---             java.setup()
---         else
---             vim.notify('nvim-java not found, please run :Pack sync', vim.log.levels.WARN)
---         end
---     end,
--- })
+-- disable chinese when learving the insert mode
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    vim.fn.jobstart({ "fcitx5-remote", "-c" }, {
+      stdout_buffered = false,
+      stderr_buffered = false,
+    })
+  end,
+})
