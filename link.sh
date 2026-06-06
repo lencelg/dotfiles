52f00b0aeaa1a1896dbd868d2d76ba5df511434a
混的人 <2223544759@qq.com>
2026-06-07 00:23:50 +0800

    rm lsd config

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dots=("qutebrowser" "foot" "niri" "waybar" "wayshot" "yazi" "kitty" "mako" "nvim" "swaylock" "weathr" "rofi" "lsd")
+dots=("qutebrowser" "foot" "niri" "waybar" "wayshot" "yazi" "kitty" "mako" "nvim" "swaylock" "weathr" "rofi" )
 BACKUP_DIR="$HOME/dotfiles_backup_$(date +%Y%m%d_%H%M%S)"
 
 # 创建备份目录
